1e489e0939cbcf5784bfcace3454c8c7ac5fc737
Thiago "Akinori" José F. de Oliveira <thiagotrigofonseca@gmail.com>
2025-10-06 22:44:29 -0300

    botoes funcionam

--- AudioManager3I/Assets/Scripts/AudioManagement.cs
+++ AudioManager3I/Assets/Scripts/AudioManagement.cs
@@ -12,12 +12,46 @@ public class AudioManagement : MonoBehaviour
     [SerializeField] private AudioClip somMarteloTribunal;
     void Start()
     {
-        
+
     }
 
-    
     void Update()
     {
-        
+
+    }
+
+    public void BotaoChoque()
+    {
+        audioSource.PlayOneShot(somChoque);
+    }
+
+    public void BotaoGrito()
+    {
+        audioSource.PlayOneShot(somGrito);
+    }
+
+    public void BotaoChicote()
+    {
+        audioSource.PlayOneShot(somChicote);
+    }
+
+    public void BotaoAfogamento()
+    {
+        audioSource.PlayOneShot(somAfogamento);
+    }
+
+    public void BotaoMurro()
+    {
+        audioSource.PlayOneShot(somMurro);
+    }
+
+    public void BotaoTapa()
+    {
+        audioSource.PlayOneShot(somTapa);
+    }
+
+    public void BotaoMarteloTribunal()
+    {
+        audioSource.PlayOneShot(somMarteloTribunal);
     }
 }
